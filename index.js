e712d8737cd85fc64e62f54f3a24bd7a9e02fc64
bdylanwalker <b.dylan.walker@gmail.com>
2016-09-27 10:51:12 -0700

    day 0

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+'use strict';
+
+//day 0
+function processData( inputString) {
+  process.stdout.write( 'Hello, World.\n');
+  process.stdout.write( inputString);
+
+}
+
+//process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+process.stdin.on('readable', ( _input) => {
+  processData( process.argv[2]);
+
+});
